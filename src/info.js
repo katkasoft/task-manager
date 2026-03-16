d5fe58722b5778d136fc2f98527bf10c3f05c6e3
katkasoft <katkaleks@gmail.com>
2026-03-16 18:28:58 +0300

    info test fix

--- src/info.js
+++ src/info.js
@@ -2,8 +2,9 @@ const { listen } = window.__TAURI__.event;
 const root = document.getElementById("root");
 
 async function init() {
-    await listen('info', async (event) => {
-        root.innerHTML = event.payload;
+    await listen('info', (event) => {
+        const data = event.payload;
+        root.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
     });
 }
 
